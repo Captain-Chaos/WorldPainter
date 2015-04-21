bfa246d8a36f99b3eb6d7a2104104bf5b1240ff8
Captain Chaos <codemaster@pepsoft.org>
2015-04-21 22:30:16 +0200

    Implement Mac support more elegantly; by using a separate utility class and compiling against support library directly instead of using reflection

---
+++ WorldPainter/WPUtils/src/main/java/org/pepsoft/util/AwtUtils.java
@@ -0,0 +1,119 @@
+/*
+ * WorldPainter, a graphical and interactive map generator for Minecraft.
+ * Copyright © 2011-2015  pepsoft.org, The Netherlands
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.pepsoft.util;
+
+import javax.swing.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.Callable;
+
+/**
+ * Utility methods for working with the AWT.
+ *
+ * Created by pepijn on 16-04-15.
+ */
+public class AwtUtils {
+    /**
+     * Execute a task on the even dispatch thread and return the result. The
+     * task <em>may</em> be executed on a different thread, so it must be
+     * thread-safe.
+     *
+     * @param task The task to execute.
+     * @param <T> The type of the result to return.
+     * @return The result of the task.
+     */
+    @SuppressWarnings("unchecked") // Responsibility of caller
+    public static <T> T resultOfOnEventThread(final Callable<T> task) {
+        if (SwingUtilities.isEventDispatchThread()) {
+            try {
+                return task.call();
+            } catch (Exception e) {
+                if (e instanceof RuntimeException) {
+                    throw (RuntimeException) e;
+                } else {
+                    throw new RuntimeException(e.getClass().getSimpleName() + " thrown by task", e);
+                }
+            }
+        } else {
+            final Object[] result = new Object[1];
+            final Exception[] exception = new Exception[1];
+            try {
+                SwingUtilities.invokeAndWait(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            synchronized (result) {
+                                result[0] = task.call();
+                            }
+                        } catch (Exception e) {
+                            synchronized (exception) {
+                                exception[0] = e;
+                            }
+                        }
+                    }
+                });
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Thread interrupted while waiting for task to execute on event dispatch thread", e);
+            } catch (InvocationTargetException e) {
+                Throwable cause = e.getTargetException();
+                throw new RuntimeException(cause.getClass().getSimpleName() + " thrown by task on event dispatch thread", cause);
+            }
+            synchronized (exception) {
+                if (exception[0] != null) {
+                    throw new RuntimeException(exception[0].getClass().getSimpleName() + " thrown by task on event dispatch thread", exception[0]);
+                }
+            }
+            synchronized (result) {
+                return (T) result[0];
+            }
+        }
+    }
+
+    /**
+     * Execute a task on the even dispatch thread and wait for it to finish. The
+     * task <em>may</em> be executed on a different thread, so it must be
+     * thread-safe.
+     *
+     * @param task The task to execute.
+     */
+    public static void doOnEventThreadAndWait(Runnable task) {
+        if (SwingUtilities.isEventDispatchThread()) {
+            task.run();
+        } else {
+            try {
+                SwingUtilities.invokeAndWait(task);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Thread interrupted while waiting for task to execute on event dispatch thread", e);
+            } catch (InvocationTargetException e) {
+                Throwable cause = e.getTargetException();
+                throw new RuntimeException(cause.getClass().getSimpleName() + " thrown by task on event dispatch thread", cause);
+            }
+        }
+    }
+
+    /**
+     * Schedule a task for later execution on the even dispatch thread and
+     * return immediately. The task <em>may</em> be executed on a different
+     * thread, so it must be thread-safe.
+     *
+     * @param task The task to execute.
+     */
+    public static void doLaterOnEventThread(Runnable task) {
+        SwingUtilities.invokeLater(task);
+    }
+}
---
+++ WorldPainter/WPUtils/src/main/java/org/pepsoft/util/MacUtils.java
@@ -0,0 +1,146 @@
+/*
+ * WorldPainter, a graphical and interactive map generator for Minecraft.
+ * Copyright © 2011-2015  pepsoft.org, The Netherlands
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.pepsoft.util;
+
+import com.apple.eawt.AppEvent;
+import com.apple.eawt.Application;
+import com.apple.eawt.QuitResponse;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * Utility methods for integrating into the Mac OS X desktop environment.
+ *
+ * Created by pepijn on 16-04-15.
+ */
+public final class MacUtils {
+    private MacUtils() {
+        // Prevent instantiation
+    }
+
+    /**
+     * When the user requests to quit the application, invokes the specified
+     * handler on the event dispatch thread and allows the quit to proceed if it
+     * returns <code>true</code> or cancels it otherwise.
+     *
+     * @param quitHandler The handler to invoke.
+     * @return <code>true</code> if the handler was successfully installed;
+     *     <code>false</code> if the Apple Java extensions cannot be found or
+     *     are too old.
+     */
+    public static boolean installQuitHandler(final QuitHandler quitHandler) {
+        Application application = Application.getApplication();
+        application.setQuitHandler(new com.apple.eawt.QuitHandler() {
+            @Override
+            public void handleQuitRequestWith(AppEvent.QuitEvent quitEvent, QuitResponse quitResponse) {
+                boolean shouldQuit = AwtUtils.resultOfOnEventThread(new Callable<Boolean>() {
+                    @Override
+                    public Boolean call() throws Exception {
+                        return quitHandler.quitRequested();
+                    }
+                });
+                if (shouldQuit) {
+                    quitResponse.performQuit();
+                } else {
+                    quitResponse.cancelQuit();
+                }
+            }
+        });
+        return true;
+    }
+
+    /**
+     * When the user requests to view the About screen, invokes the specified
+     * handler on the event dispatch thread.
+     *
+     * @param aboutHandler The handler to invoke.
+     * @return <code>true</code> if the handler was successfully installed;
+     *     <code>false</code> if the Apple Java extensions cannot be found or
+     *     are too old.
+     */
+    public static boolean installAboutHandler(final AboutHandler aboutHandler) {
+        Application application = Application.getApplication();
+        application.setAboutHandler(new com.apple.eawt.AboutHandler() {
+            @Override
+            public void handleAbout(AppEvent.AboutEvent aboutEvent) {
+                AwtUtils.doLaterOnEventThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        aboutHandler.aboutRequested();
+                    }
+                });
+            }
+        });
+        return true;
+    }
+
+    /**
+     * When the user requests to open (a) file(s) associated with the
+     * application, invokes the specified handler on the event dispatch thread.
+     *
+     * @param openFilesHandler The handler to invoke.
+     * @return <code>true</code> if the handler was successfully installed;
+     *     <code>false</code> if the Apple Java extensions cannot be found or
+     *     are too old.
+     */
+    public static boolean installOpenFilesHandler(final OpenFilesHandler openFilesHandler) {
+        Application application = Application.getApplication();
+        application.setOpenFileHandler(new com.apple.eawt.OpenFilesHandler() {
+            @Override
+            public void openFiles(AppEvent.OpenFilesEvent openFilesEvent) {
+                final List<File> files = openFilesEvent.getFiles();
+                AwtUtils.doLaterOnEventThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        openFilesHandler.filesOpened(files);
+                    }
+                });
+            }
+        });
+        return true;
+    }
+
+    public interface QuitHandler {
+        /**
+         * Invoked when the user has requested to quit the application.
+         *
+         * @return <code>true</code> if the quit should proceed.
+         */
+        boolean quitRequested();
+    }
+
+    public interface AboutHandler {
+        /**
+         * Invoked when the user has requested to view the About screen.
+         */
+        void aboutRequested();
+    }
+
+    public interface OpenFilesHandler {
+        /**
+         * Invoked when the user has requested to open (a) file(s) associated
+         * with this application.
+         *
+         * @param files The list of files requested to be opened.
+         */
+        void filesOpened(List<File> files);
+    }
+}
