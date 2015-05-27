290629b8d6325e4510eec09df5553f7846a2616f
Pepijn Schmitz <Pepijn.Schmitz@pepsoft.org>
2015-05-27 15:59:23 +0200

    Document ColourScheme

--- WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/ColourScheme.java
+++ WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/ColourScheme.java
@@ -7,11 +7,35 @@ package org.pepsoft.worldpainter;
 import org.pepsoft.minecraft.Material;
 
 /**
+ * A provider of colours of Minecraft blocks. The colours are specified as <code>int</code>s in packed RGB format, that
+ * is to say bits 0 through 7 specify the blue component as a number from 0 to 255 (inclusive); bits 8 through 15 the
+ * green component and bits 16 through 23 the red component.
  *
  * @author pepijn
  */
 public interface ColourScheme {
+    /**
+     * Get the colour of a particular Minecraft block type.
+     *
+     * @param blockType The block type ID for which to get the colour.
+     * @return The colour of the specified block type in packed RGB format.
+     */
     int getColour(int blockType);
+
+    /**
+     * Get the colour of a particular Minecraft block type with a particular data value.
+     *
+     * @param blockType The block type ID for which to get the colour.
+     * @param dataValue The data value for which to get the colour.
+     * @return The colour of the specified block type in packed RGB format.
+     */
     int getColour(int blockType, int dataValue);
+
+    /**
+     * Get the colour of a particular Minecraft material.
+     *
+     * @param material The material for which to get the colour.
+     * @return The colour of the specified material in packed RGB format.
+     */
     int getColour(Material material);
 }
