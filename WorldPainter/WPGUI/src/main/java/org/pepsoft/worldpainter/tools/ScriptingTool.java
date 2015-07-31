c1be17d3f72b8105813a32410f1b397d58e8c16f
Captain Chaos <codemaster@pepsoft.org>
2015-07-31 18:40:01 +0200

    Move ScriptingTool to WPGUI (because it uses Logback directly to configure logging and I don't want to make WPCore dependent on that)

--- WorldPainter/WPGUI/src/main/java/org/pepsoft/worldpainter/tools/ScriptingTool.java
+++ WorldPainter/WPGUI/src/main/java/org/pepsoft/worldpainter/tools/ScriptingTool.java
@@ -6,11 +6,16 @@
 
 package org.pepsoft.worldpainter.tools;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
 import org.pepsoft.util.PluginManager;
 import org.pepsoft.worldpainter.Configuration;
 import org.pepsoft.worldpainter.Version;
 import org.pepsoft.worldpainter.plugins.WPPluginManager;
 import org.pepsoft.worldpainter.tools.scripts.ScriptingContext;
+import org.slf4j.LoggerFactory;
 
 import javax.script.*;
 import java.io.File;
@@ -19,8 +24,6 @@ import java.io.IOException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -29,8 +32,16 @@ import java.util.logging.Logger;
 public class ScriptingTool {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         // Initialise logging
-        System.setProperty("java.util.logging.SimpleFormatter.format", "[%4$s] %5$s%6$s%n");
-        Logger.getLogger("").setLevel(Level.WARNING);
+        LoggerContext logContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        try {
+            JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(logContext);
+            logContext.reset();
+            configurator.doConfigure(ClassLoader.getSystemResourceAsStream("/logback-scriptingtool.xml"));
+        } catch (JoranException e) {
+            // StatusPrinter will handle this
+        }
+        StatusPrinter.printInCaseOfErrorsOrWarnings(logContext);
 
         System.err.println("WorldPainter scripting host version " + Version.VERSION + ".\n" +
                 "Copyright 2011-2015 pepsoft.org, The Netherlands.\n" +
@@ -85,7 +96,7 @@ public class ScriptingTool {
             CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
             trustedCert = (X509Certificate) certificateFactory.generateCertificate(ClassLoader.getSystemResourceAsStream("wproot.pem"));
         } catch (CertificateException e) {
-            logger.log(Level.SEVERE, "Certificate exception while loading trusted root certificate", e);
+            logger.error("Certificate exception while loading trusted root certificate", e);
         }
         
         // Load the plugins
@@ -94,7 +105,7 @@ public class ScriptingTool {
             if (trustedCert != null) {
                 PluginManager.loadPlugins(pluginsDir, trustedCert.getPublicKey());
             } else {
-                logger.severe("Trusted root certificate not available; not loading plugins");
+                logger.error("Trusted root certificate not available; not loading plugins");
             }
         }
         WPPluginManager.initialise(config.getUuid());
@@ -129,13 +140,13 @@ public class ScriptingTool {
             // Check that go() was invoked on the last operation:
             context.checkGoCalled(null);
         } catch (RuntimeException e) {
-            logger.log(Level.SEVERE, e.getClass().getSimpleName() + " occurred while executing " + scriptFileName, e);
+            logger.error(e.getClass().getSimpleName() + " occurred while executing " + scriptFileName, e);
             System.exit(2);
         } catch (ScriptException e) {
-            logger.log(Level.SEVERE, "ScriptException occurred while executing " + scriptFileName, e);
+            logger.error("ScriptException occurred while executing " + scriptFileName, e);
             System.exit(2);
         }
     }
     
-    private static final Logger logger = Logger.getLogger(ScriptingTool.class.getName());
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ScriptingTool.class);
 }
