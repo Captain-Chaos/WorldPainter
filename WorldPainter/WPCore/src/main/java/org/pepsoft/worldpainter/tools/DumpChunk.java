755ef1bb86b3e6313c03978aa2a789bf5778f2ab
Captain Chaos <codemaster@pepsoft.org>
2015-08-01 20:31:04 +0200

    Show information about tile entity data

--- WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/tools/DumpChunk.java
+++ WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/tools/DumpChunk.java
@@ -10,7 +10,10 @@ import org.pepsoft.minecraft.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
+import static org.pepsoft.minecraft.Block.BLOCKS;
 import static org.pepsoft.minecraft.Block.BLOCK_TYPE_NAMES;
 import static org.pepsoft.minecraft.Constants.SUPPORTED_VERSION_1;
 
@@ -31,6 +34,7 @@ public class DumpChunk {
         Chunk chunk = (level.getVersion() == SUPPORTED_VERSION_1)
                 ? new ChunkImpl(tag, level.getMaxHeight())
                 : new ChunkImpl2(tag, level.getMaxHeight());
+        List<TileEntity> tileEntities = chunk.getTileEntities();
         for (int y = 0; y < level.getMaxHeight(); y++) {
             boolean blockFound = false;
 x:          for (int x = 0; x < 16; x++) {
@@ -50,10 +54,30 @@ x:          for (int x = 0; x < 16; x++) {
                     int blockType = chunk.getBlockType(x, y, z);
                     int data = chunk.getDataValue(x, y, z);
                     if (blockType > 0) {
-                        if (data > 0) {
-                            System.out.printf("[%3.3s:%2d]", BLOCK_TYPE_NAMES[blockType], data);
+                        if (BLOCKS[blockType].tileEntity) {
+                            int count = 0;
+                            for (Iterator<TileEntity> i = tileEntities.iterator(); i.hasNext(); ) {
+                                TileEntity tileEntity = i.next();
+                                if ((tileEntity.getX() == x) && (tileEntity.getY() == y) && (tileEntity.getZ() == z)) {
+                                    count++;
+                                    i.remove();
+                                }
+                            }
+                            if (count == 1) {
+                                if (data > 0) {
+                                    System.out.printf("[%3.3s:%2d]", BLOCK_TYPE_NAMES[blockType], data);
+                                } else {
+                                    System.out.printf("[%3.3s:  ]", BLOCK_TYPE_NAMES[blockType]);
+                                }
+                            } else {
+                                System.out.printf("!%3.3s!%2d!", BLOCK_TYPE_NAMES[blockType], count);
+                            }
                         } else {
-                            System.out.printf("[%3.3s:  ]", BLOCK_TYPE_NAMES[blockType]);
+                            if (data > 0) {
+                                System.out.printf("[%3.3s:%2d]", BLOCK_TYPE_NAMES[blockType], data);
+                            } else {
+                                System.out.printf("[%3.3s:  ]", BLOCK_TYPE_NAMES[blockType]);
+                            }
                         }
                     } else {
                         System.out.print("[   :  ]");
@@ -69,6 +93,12 @@ x:          for (int x = 0; x < 16; x++) {
                     System.out.print(" Y: " + y);
                 }
                 System.out.println();
+            }
+        }
+        if (! tileEntities.isEmpty()) {
+            System.out.println("Unmatched tile entities!");
+            for (TileEntity tileEntity: tileEntities) {
+                System.out.println(tileEntity.getId() + "@" + tileEntity.getX() + "," + tileEntity.getY() + "," + tileEntity.getZ());
             }
         }
     }
