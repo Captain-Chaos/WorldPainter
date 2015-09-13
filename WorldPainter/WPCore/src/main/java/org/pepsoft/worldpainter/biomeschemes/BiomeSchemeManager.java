0409b268e6596c332ce36bbf6ddd537f2a5d881c
Captain Chaos <codemaster@pepsoft.org>
2015-09-13 20:33:13 +0200

    Add support for Minecraft 1.8.8 jar

--- WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/biomeschemes/BiomeSchemeManager.java
+++ WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/biomeschemes/BiomeSchemeManager.java
@@ -86,10 +86,10 @@ public class BiomeSchemeManager {
                     version = "1.6.4 or 1.3.1 - 1.6.2";
                     break;
                 case BIOME_ALGORITHM_1_7_DEFAULT:
-                    version = "1.8.3 or 1.7.2 - 1.8.1";
+                    version = "1.8.8 or 1.7.2 - 1.8.3";
                     break;
                 case BIOME_ALGORITHM_1_7_LARGE:
-                    version = "1.8.3 or 1.7.2 - 1.8.1";
+                    version = "1.8.8 or 1.7.2 - 1.8.3";
                     break;
                 default:
                     throw new IllegalArgumentException();
@@ -435,12 +435,14 @@ public class BiomeSchemeManager {
         addDescriptor(new Checksum(new byte[] {(byte) -122, (byte) 99, (byte) -95, (byte) 12, (byte) -20, (byte) -63, (byte) 14, (byte) -86, (byte) 104, (byte) 58, (byte) -110, (byte) 126, (byte) -11, (byte) 55, (byte) 24, (byte) 82}),     new BiomeSchemeDescriptor(new Version(1, 8),    BIOME_ALGORITHM_1_7_DEFAULT, Minecraft1_8BiomeScheme.class, true));
         addDescriptor(new Checksum(new byte[] {(byte) 92, (byte) -102, (byte) -81, (byte) 49, (byte) 25, (byte) -97, (byte) 118, (byte) 62, (byte) -7, (byte) 8, (byte) 92, (byte) -55, (byte) -74, (byte) -112, (byte) 43, (byte) 29}),        new BiomeSchemeDescriptor(new Version(1, 8, 1), BIOME_ALGORITHM_1_7_DEFAULT, Minecraft1_8BiomeScheme.class, true));
         addDescriptor(new Checksum(new byte[] {(byte) -108, (byte) 55, (byte) -76, (byte) -114, (byte) 5, (byte) 27, (byte) 12, (byte) -24, (byte) -127, (byte) 124, (byte) -104, (byte) -98, (byte) 89, (byte) -25, (byte) -103, (byte) 79}),  new BiomeSchemeDescriptor(new Version(1, 8, 3), BIOME_ALGORITHM_1_7_DEFAULT, Minecraft1_8BiomeScheme.class, true));
+        addDescriptor(new Checksum(new byte[] {(byte) 101, (byte) -123, (byte) -119, (byte) 23, (byte) -111, (byte) -32, (byte) -41, (byte) 93, (byte) -59, (byte) 76, (byte) -5, (byte) 84, (byte) 122, (byte) -122, (byte) 109, (byte) -80}), new BiomeSchemeDescriptor(new Version(1, 8, 8), BIOME_ALGORITHM_1_7_DEFAULT, Minecraft1_8BiomeScheme.class, true));
 
         addDescriptor(new Checksum(new byte[] {(byte) 122, (byte) 48, (byte) 69, (byte) 84, (byte) -3, (byte) -22, (byte) -121, (byte) -102, (byte) 121, (byte) -98, (byte) -2, (byte) 110, (byte) -82, (byte) -35, (byte) -116, (byte) -107}), new BiomeSchemeDescriptor(new Version(1, 7, 2), BIOME_ALGORITHM_1_7_LARGE, Minecraft1_7LargeBiomeScheme.class, true));
         addDescriptor(new Checksum(new byte[] {(byte) 95, (byte) 124, (byte) -57, (byte) -21, (byte) 1, (byte) -53, (byte) -39, (byte) 53, (byte) -87, (byte) -105, (byte) 60, (byte) -74, (byte) -23, (byte) -60, (byte) -63, (byte) 14}),     new BiomeSchemeDescriptor(new Version(1, 7, 9), BIOME_ALGORITHM_1_7_LARGE, Minecraft1_7LargeBiomeScheme.class, true));
         addDescriptor(new Checksum(new byte[] {(byte) -122, (byte) 99, (byte) -95, (byte) 12, (byte) -20, (byte) -63, (byte) 14, (byte) -86, (byte) 104, (byte) 58, (byte) -110, (byte) 126, (byte) -11, (byte) 55, (byte) 24, (byte) 82}),     new BiomeSchemeDescriptor(new Version(1, 8),    BIOME_ALGORITHM_1_7_LARGE, Minecraft1_8LargeBiomeScheme.class, true));
         addDescriptor(new Checksum(new byte[] {(byte) 92, (byte) -102, (byte) -81, (byte) 49, (byte) 25, (byte) -97, (byte) 118, (byte) 62, (byte) -7, (byte) 8, (byte) 92, (byte) -55, (byte) -74, (byte) -112, (byte) 43, (byte) 29}),        new BiomeSchemeDescriptor(new Version(1, 8, 1), BIOME_ALGORITHM_1_7_LARGE, Minecraft1_8LargeBiomeScheme.class, true));
         addDescriptor(new Checksum(new byte[] {(byte) -108, (byte) 55, (byte) -76, (byte) -114, (byte) 5, (byte) 27, (byte) 12, (byte) -24, (byte) -127, (byte) 124, (byte) -104, (byte) -98, (byte) 89, (byte) -25, (byte) -103, (byte) 79}),  new BiomeSchemeDescriptor(new Version(1, 8, 3), BIOME_ALGORITHM_1_7_LARGE, Minecraft1_8LargeBiomeScheme.class, true));
+        addDescriptor(new Checksum(new byte[] {(byte) 101, (byte) -123, (byte) -119, (byte) 23, (byte) -111, (byte) -32, (byte) -41, (byte) 93, (byte) -59, (byte) 76, (byte) -5, (byte) 84, (byte) 122, (byte) -122, (byte) 109, (byte) -80}), new BiomeSchemeDescriptor(new Version(1, 8, 8), BIOME_ALGORITHM_1_7_LARGE, Minecraft1_8LargeBiomeScheme.class, true));
     }
 
     private static void addDescriptor(Checksum checksum, BiomeSchemeDescriptor descriptor) {
--- WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/biomeschemes/Minecraft1_8LargeBiomeScheme.java
+++ WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/biomeschemes/Minecraft1_8LargeBiomeScheme.java
@@ -17,12 +17,12 @@ import org.pepsoft.util.Checksum;
  */
 public final class Minecraft1_8LargeBiomeScheme extends Minecraft1_8JarBiomeScheme {
     public Minecraft1_8LargeBiomeScheme(File minecraftJar, File libDir, Checksum md5Sum) {
-        super(minecraftJar, libDir, md5Sum, HASHES_TO_CLASSNAMES, "1.8.3 Large Biomes");
+        super(minecraftJar, libDir, md5Sum, HASHES_TO_CLASSNAMES, "1.8.8 Large Biomes");
         try {
             Field field = worldGeneratorClass.getField("d");
             largeBiomesGenerator = field.get(null);
         } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException("Not a valid 1.8.3 or 1.8 to 1.8.1 minecraft.jar", e);
+            throw new RuntimeException("Not a valid 1.8.8 or 1.8 to 1.8.3 minecraft.jar", e);
         }
     }
     
@@ -44,12 +44,13 @@ public final class Minecraft1_8LargeBiomeScheme extends Minecraft1_8JarBiomeSche
     
     private static final Map<Checksum, String[]> HASHES_TO_CLASSNAMES = new HashMap<>();
 
-//                                                                                                                                                                                                                                                                      Landscape class
-//                                                                                                                                                                                                                                                                             Buffer manager class
-//                                                                                                                                                                                                                                                                                    World generator class
-    static { //                                                                                                                                                                                                                                                                              Initialiser class
-        HASHES_TO_CLASSNAMES.put(new Checksum(new byte[] {(byte) -122, (byte) 99, (byte) -95, (byte) 12, (byte) -20, (byte) -63, (byte) 14, (byte) -86, (byte) 104, (byte) 58, (byte) -110, (byte) 126, (byte) -11, (byte) 55, (byte) 24, (byte) 82}),    new String[] {"bpa", "boy", "are", "od"}); // 1.8
-        HASHES_TO_CLASSNAMES.put(new Checksum(new byte[] {(byte) 92, (byte) -102, (byte) -81, (byte) 49, (byte) 25, (byte) -97, (byte) 118, (byte) 62, (byte) -7, (byte) 8, (byte) 92, (byte) -55, (byte) -74, (byte) -112, (byte) 43, (byte) 29}),       new String[] {"boy", "bow", "arb", "oe"}); // 1.8.1
-        HASHES_TO_CLASSNAMES.put(new Checksum(new byte[] {(byte) -108, (byte) 55, (byte) -76, (byte) -114, (byte) 5, (byte) 27, (byte) 12, (byte) -24, (byte) -127, (byte) 124, (byte) -104, (byte) -98, (byte) 89, (byte) -25, (byte) -103, (byte) 79}), new String[] {"asf", "asd", "ads", "kc"}); // 1.8.3
+//                                                                                                                                                                                                                                                                       Landscape class
+//                                                                                                                                                                                                                                                                              Buffer manager class
+//                                                                                                                                                                                                                                                                                     World generator class
+    static { //                                                                                                                                                                                                                                                                               Initialiser class
+        HASHES_TO_CLASSNAMES.put(new Checksum(new byte[] {(byte) -122, (byte) 99, (byte) -95, (byte) 12, (byte) -20, (byte) -63, (byte) 14, (byte) -86, (byte) 104, (byte) 58, (byte) -110, (byte) 126, (byte) -11, (byte) 55, (byte) 24, (byte) 82}),     new String[] {"bpa", "boy", "are", "od"}); // 1.8
+        HASHES_TO_CLASSNAMES.put(new Checksum(new byte[] {(byte) 92, (byte) -102, (byte) -81, (byte) 49, (byte) 25, (byte) -97, (byte) 118, (byte) 62, (byte) -7, (byte) 8, (byte) 92, (byte) -55, (byte) -74, (byte) -112, (byte) 43, (byte) 29}),        new String[] {"boy", "bow", "arb", "oe"}); // 1.8.1
+        HASHES_TO_CLASSNAMES.put(new Checksum(new byte[] {(byte) -108, (byte) 55, (byte) -76, (byte) -114, (byte) 5, (byte) 27, (byte) 12, (byte) -24, (byte) -127, (byte) 124, (byte) -104, (byte) -98, (byte) 89, (byte) -25, (byte) -103, (byte) 79}),  new String[] {"asf", "asd", "ads", "kc"}); // 1.8.3
+        HASHES_TO_CLASSNAMES.put(new Checksum(new byte[] {(byte) 101, (byte) -123, (byte) -119, (byte) 23, (byte) -111, (byte) -32, (byte) -41, (byte) 93, (byte) -59, (byte) 76, (byte) -5, (byte) 84, (byte) 122, (byte) -122, (byte) 109, (byte) -80}), new String[] {"ase", "asc", "adr", "kb"}); // 1.8.8
     }
 }
