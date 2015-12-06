46c5cf98f7337634f5c58b86be21b84e2d24474b
Captain Chaos <codemaster@pepsoft.org>
2015-12-06 18:26:36 +0100

    Work in progress on running scripts (add configurable description, parameters)

--- WorldPainter/WPGUI/src/main/java/org/pepsoft/worldpainter/tools/scripts/ScriptRunner.java
+++ WorldPainter/WPGUI/src/main/java/org/pepsoft/worldpainter/tools/scripts/ScriptRunner.java
@@ -5,24 +5,28 @@
  */
 package org.pepsoft.worldpainter.tools.scripts;
 
-import java.awt.Window;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import javax.script.Bindings;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileFilter;
+
+import org.jetbrains.annotations.NotNull;
 import org.pepsoft.util.FileUtils;
 import org.pepsoft.worldpainter.Configuration;
 import org.pepsoft.worldpainter.Dimension;
@@ -49,6 +53,9 @@ public class ScriptRunner extends WorldPainterDialog {
         Configuration config = Configuration.getInstance();
         recentScriptFiles = (config.getRecentScriptFiles() != null) ? new ArrayList(config.getRecentScriptFiles()) : new ArrayList<>();
         jComboBox1.setModel(new DefaultComboBoxModel<>(recentScriptFiles.toArray(new File[recentScriptFiles.size()])));
+        if (jComboBox1.getSelectedItem() != null) {
+            setupScript((File) jComboBox1.getSelectedItem());
+        }
         setControlStates();
         
         getRootPane().setDefaultButton(jButton2);
@@ -92,6 +99,120 @@ public class ScriptRunner extends WorldPainterDialog {
             recentScriptFiles.add(0, script);
             jComboBox1.setModel(new DefaultComboBoxModel<>(recentScriptFiles.toArray(new File[recentScriptFiles.size()])));
             jComboBox1.setSelectedItem(script);
+            setupScript(script);
+        }
+    }
+
+    private void setupScript(File script) {
+        ScriptDescriptor descriptor = analyseScript(script);
+        
+        // Remove any previously added fields:
+        while (panelDescriptor.getComponentCount() > 2) {
+            panelDescriptor.remove(2);
+        }
+        
+        // If there is a descriptor, use it to add fields for the parameters:
+        if (descriptor != null) {
+            if (descriptor.name != null) {
+                labelName.setText(script.getName());
+            } else {
+                labelName.setText(script.getName());
+            }
+            if (descriptor.description != null) {
+                addRegular(panelDescriptor, new JLabel("Description:"));
+                addlastOnLine(panelDescriptor, new JTextArea(descriptor.description));
+            }
+            for (ParameterDescriptor paramDescriptor: descriptor.parameterDescriptors) {
+                JLabel label = new JLabel(paramDescriptor.name + ':');
+                JTextField field = new JTextField();
+                label.setLabelFor(field);
+                if (paramDescriptor.description != null) {
+                    label.setToolTipText(paramDescriptor.description);
+                }
+                addRegular(panelDescriptor, label);
+                if (paramDescriptor.description != null) {
+                    field.setToolTipText(paramDescriptor.description);
+                }
+                addlastOnLine(panelDescriptor, field);
+            }
+        } else {
+            labelName.setText(script.getName());
+        }
+    }
+    
+    private void addRegular(JPanel panel, JComponent component) {
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        constraints.insets = new Insets(2, 0, 2, 2);
+        panel.add(component, constraints);
+    }
+
+    private void addlastOnLine(JPanel panel, JComponent component) {
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        constraints.insets = new Insets(2, 2, 2, 0);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 1.0;
+        constraints.gridwidth = GridBagConstraints.REMAINDER;
+        panel.add(component, constraints);
+    }
+    
+    private ScriptDescriptor analyseScript(File script) {
+        Properties properties = new Properties();
+        try (BufferedReader in = new BufferedReader(new FileReader(script))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                line = line.trim();
+                if (line.isEmpty()) {
+                    continue;
+                } else if (line.startsWith("#") || line.startsWith("//")) {
+                    Matcher matcher = DESCRIPTOR_PATTERN.matcher(line);
+                    if (matcher.find()) {
+                        properties.put(matcher.group(1), matcher.group(2));
+                    }
+                } else {
+                    // Stop after the first non-comment and non-empty line
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("I/O error reading script " + script);
+        }
+        if (properties.isEmpty()) {
+            return null;
+        } else {
+            ScriptDescriptor descriptor = new ScriptDescriptor();
+            Map<String, ParameterDescriptor> paramMap = new HashMap<>();
+            properties.forEach((keyObject, valueObject) -> {
+                String key = (String) keyObject, value = (String) valueObject;
+                if (key.equals("name")) {
+                    descriptor.name = value;
+                } else if (key.equals("description")) {
+                    descriptor.description = value.replace("\\n", "\n");
+                } else if (key.startsWith("param.")) {
+                    String[] parts = key.split("\\.");
+                    if (parts.length != 3) {
+                        logger.warn("Skipping invalid key \"" + key + "\" in script descriptor");
+                    }
+                    ParameterDescriptor paramDescriptor = paramMap.get(parts[1]);
+                    if (paramDescriptor == null) {
+                        paramDescriptor = new ParameterDescriptor();
+                        paramDescriptor.name = parts[1];
+                        paramMap.put(parts[1], paramDescriptor);
+                        descriptor.parameterDescriptors.add(paramDescriptor);
+                    }
+                    if (parts[2].equals("type")) {
+                        paramDescriptor.type = parts[2];
+                    } else if (parts[2].equals("description")) {
+                        paramDescriptor.description = parts[2].replace("\\n", "\n");
+                    } else {
+                        logger.warn("Skipping unknown key \"" + key + "\" in script descriptor");
+                    }
+                } else {
+                    logger.warn("Skipping unknown key \"" + key + "\" in script descriptor");
+                }
+            });
+            return descriptor;
         }
     }
 
@@ -101,6 +222,7 @@ public class ScriptRunner extends WorldPainterDialog {
         jTextArea1.setEnabled(false);
         jButton2.setEnabled(false);
         jButton3.setEnabled(false);
+        jTextArea2.setText(null);
         new Thread() {
             @Override
             public void run() {
@@ -135,8 +257,9 @@ public class ScriptRunner extends WorldPainterDialog {
                     // Capture output
                     scriptEngine.getContext().setWriter(new Writer() {
                         @Override
-                        public void write(char[] cbuf, int off, int len) throws IOException {
-                            SwingUtilities.invokeLater(() -> jTextArea2.append(new String(cbuf, off, len)));
+                        public void write(@NotNull char[] cbuf, int off, int len) throws IOException {
+                            String text = new String(cbuf, off, len);
+                            SwingUtilities.invokeLater(() -> jTextArea2.append(text));
                         }
 
                         @Override public void flush() throws IOException {}
@@ -180,6 +303,10 @@ public class ScriptRunner extends WorldPainterDialog {
                     }
                 } finally {
                     SwingUtilities.invokeLater(() -> {
+                        jComboBox1.setEnabled(true);
+                        jButton1.setEnabled(true);
+                        jTextArea1.setEnabled(true);
+                        jButton2.setEnabled(true);
                         jButton3.setText("Close");
                         jButton3.setEnabled(true);
                     });
@@ -196,6 +323,7 @@ public class ScriptRunner extends WorldPainterDialog {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
         jLabel1 = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox();
@@ -209,6 +337,9 @@ public class ScriptRunner extends WorldPainterDialog {
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextArea2 = new javax.swing.JTextArea();
         jButton3 = new javax.swing.JButton();
+        panelDescriptor = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        labelName = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setTitle("Run Script");
@@ -260,34 +391,51 @@ public class ScriptRunner extends WorldPainterDialog {
             }
         });
 
+        panelDescriptor.setLayout(new java.awt.GridBagLayout());
+
+        jLabel5.setText("Name:");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.insets = new java.awt.Insets(0, 1, 0, 2);
+        panelDescriptor.add(jLabel5, gridBagConstraints);
+
+        labelName.setText("jLabel6");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(0, 2, 0, 0);
+        panelDescriptor.add(labelName, gridBagConstraints);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane2)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton1))
-                    .addGroup(layout.createSequentialGroup()
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
                             .addComponent(jLabel3))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 598, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel4)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(jScrollPane2)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jButton2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton3)))
+                        .addComponent(jButton3))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(panelDescriptor, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -298,7 +446,9 @@ public class ScriptRunner extends WorldPainterDialog {
                     .addComponent(jLabel1)
                     .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(18, 18, 18)
+                .addComponent(panelDescriptor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2)
@@ -308,7 +458,7 @@ public class ScriptRunner extends WorldPainterDialog {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 243, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton2)
@@ -328,6 +478,7 @@ public class ScriptRunner extends WorldPainterDialog {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        setupScript((File) jComboBox1.getSelectedItem());
         setControlStates();
     }//GEN-LAST:event_jComboBox1ActionPerformed
 
@@ -344,16 +495,31 @@ public class ScriptRunner extends WorldPainterDialog {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JLabel labelName;
+    private javax.swing.JPanel panelDescriptor;
     // End of variables declaration//GEN-END:variables
 
     private final ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
     private final World2 world;
     private final Dimension dimension;
     private final ArrayList<File> recentScriptFiles;
-    
+
+    private static final Pattern DESCRIPTOR_PATTERN = Pattern.compile("script\\.([.a-zA-Z_0-9]+)=(.+)$");
     private static final Logger logger = LoggerFactory.getLogger(ScriptRunner.class);
+
+    static class ScriptDescriptor {
+        String name, description;
+        List<ParameterDescriptor> parameterDescriptors = new ArrayList<>();
+    }
+
+    static class ParameterDescriptor {
+        String name, type, description;
+    }
+
+    enum ParameterType {STRING, INTEGER, PERCENTAGE, FLOAT, FILE}
 }
