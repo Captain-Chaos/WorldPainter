de9293c5a106b589586ae895930bd9936bd1d7b1
Captain Chaos <codemaster@pepsoft.org>
2016-01-04 21:38:08 +0100

    Change the language of the donation page to English

--- WorldPainter/WPGUI/src/main/java/org/pepsoft/worldpainter/AboutDialog.java
+++ WorldPainter/WPGUI/src/main/java/org/pepsoft/worldpainter/AboutDialog.java
@@ -145,7 +145,7 @@ public class AboutDialog extends javax.swing.JDialog implements WindowListener {
     
     private void donate() {
         try {
-            DesktopUtils.open(new URL("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VZ7WNQVPXDZHY"));
+            DesktopUtils.open(new URL("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VZ7WNQVPXDZHY&lc=US"));
             Configuration config = Configuration.getInstance();
             config.setDonationStatus(Configuration.DonationStatus.DONATED);
             JOptionPane.showMessageDialog(this, strings.getString("the.donation.paypal.page.has.been.opened"), strings.getString("thank.you"), JOptionPane.INFORMATION_MESSAGE);
