fb9b69be1ccf4314ce88733270c5a4a5abe3d670
Jeff Chen <frogocomics@gmail.com>
2016-09-06 17:59:15 +0800

    Add Javadocs

--- WorldPainter/WPCore/src/main/java/com/gmail/frogocomics/Task.java
+++ WorldPainter/WPCore/src/main/java/com/gmail/frogocomics/Task.java
@@ -1,27 +1,79 @@
 package com.gmail.frogocomics;
 
 /**
+ * Represents a task to be executed in order in the {@link TaskManager}. Tasks consists of a
+ * {@link Thread} and a name (To display on the gui), but they do not function like threads as the
+ * task manager does not allow them to run at the same time. Tasks can consist of anything, such as
+ * (mostly) global operations, or perhaps brush operations.
+ * <p><code>
+ *     <i>//Create a task</i>
+ *     Task task = new Thread(() -> {
+ *         <i>//Do whatever</i>
+ *     }).addToQueue();
+ *     TaskManager.run();
+ * </code></p>
  *
  * @author Jeff Chen
  */
 public class Task {
 
     private Thread task;
+    private String name;
 
+    /**
+     * Create a new task.
+     *
+     * @param task The task to execute in the {@link TaskManager}.
+     */
     public Task(Thread task) {
-        this.task = task;
+        new Task(task, "Unknown Task");
     }
 
+    /**
+     * Create a new task.
+     *
+     * @param task The task to execute in the {@link TaskManager}.
+     * @param name The name of the task to display in the WorldPainter gui.
+     */
+    public Task(Thread task, String name) {
+        this.task = task;
+        this.name = name;
+    }
+
+    /**
+     * Add the task to the task manager.
+     *
+     * @return Returns this class, for chaining.
+     */
     public Task addToQueue() {
         TaskManager.addTask(this);
         return this;
     }
 
+    /**
+     * Remove the task to the task manager.
+     *
+     * @return Returns this class, for chaining.
+     */
     public Task removeFromQueue() {
         TaskManager.removeTask(this);
         return this;
     }
 
+    /**
+     * Get the name of this task, to display in the WorldPainter graphical user interface.
+     *
+     * @return Returns the name of this task.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Get the thread to execute. Only {@link TaskManager} should be using this method.
+     *
+     * @return Returns the thread used to execute this task
+     */
     protected Thread getThread() {
         return task;
     }
--- WorldPainter/WPCore/src/main/java/com/gmail/frogocomics/TaskManager.java
+++ WorldPainter/WPCore/src/main/java/com/gmail/frogocomics/TaskManager.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 import java.util.Queue;
 
 /**
+ * The task manager stores loaded tasks and has options to execute them in order and add/remove
+ * any, if needed.
  *
  * @author Jeff Chen
  */
@@ -14,23 +16,72 @@ public class TaskManager {
     private TaskManager() {
     }
 
+    /**
+     * Add a task to the end of the queue.
+     *
+     * @param task The task to add
+     */
     protected static void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Remove a specific task from the queue, provided that it exists.
+     *
+     * @param task The task to remove, if it exists
+     */
     protected static void removeTask(Task task) {
         tasks.remove(task);
     }
 
+    /**
+     * Get the first task from the queue, without removing it.
+     *
+     * @return Returns the first task
+     */
     public static Optional<Task> peek() {
         return Optional.of(tasks.peek());
     }
 
+    /**
+     * Gets and removes the first task from the queue.
+     *
+     * @return Returns the first task
+     */
     public static Optional<Task> poll() {
         return Optional.of(tasks.poll());
     }
 
-   public static void run() throws InterruptedException {
+    /**
+     * Gets all the current tasks ready to be executed.
+     *
+     * @return Returns the tasks if there is any. If there isn't, {@link Optional#empty()} will
+     *         be returned.
+     */
+    public static Optional<Queue<Task>> getTasks() {
+        if (tasks.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(tasks);
+    }
+
+    /**
+     * Run one task.
+     *
+     * @throws InterruptedException When a task thread is interrupted
+     */
+    public static void runOne() throws InterruptedException {
+        Task t = tasks.poll();
+        t.getThread().start();
+        t.getThread().join();
+    }
+
+    /**
+     * Runs all tasks in order and clears the queue.
+     *
+     * @throws InterruptedException When a task thread is interrupted
+     */
+   public static void runAll() throws InterruptedException {
        for(Task t : tasks) {
            t.getThread().run();
            t.getThread().join();
