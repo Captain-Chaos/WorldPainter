35f8fbfda5437b4d6155f97b8480a50f5316860f
Captain Chaos <codemaster@pepsoft.org>
2018-05-22 13:11:36 +0200

    Add javadoc

--- WorldPainter/WPGUI/src/main/java/org/pepsoft/worldpainter/WorldPainterDialog.java
+++ WorldPainter/WPGUI/src/main/java/org/pepsoft/worldpainter/WorldPainterDialog.java
@@ -15,6 +15,18 @@ import javax.swing.JDialog;
 import javax.swing.KeyStroke;
 
 /**
+ * A WorldPainter dialog window. Provides the following features:
+ *
+ * <ul><li>Always application modal
+ * <li>A {@link #cancel()} method which dismisses the dialog programmatically
+ * <li>The dialog is also cancelled if the user presses the <code>Esc</code> key
+ * <li>An {@link #isCancelled()} method which indicates whether the dialog has
+ * been cancelled or closed in any other way besides invoking the {@link #ok()}
+ * method
+ * <li>An {@link #ok()} method which dismisses the dialog programmatically and
+ * clears the <code>cancelled</code> property
+ * <li>[SAFE MODE] automatically appended to title if WorldPainter is running in
+ * safe mode</ul>
  *
  * @author pepijn
  */
