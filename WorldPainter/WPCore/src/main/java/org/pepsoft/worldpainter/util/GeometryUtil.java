5d159310ec034ebd1e8011b72bf0f86b325c8b64
Captain Chaos <codemaster@pepsoft.org>
2018-06-01 10:26:37 +0200

    Refactor visiting of and calculation with lines to utility methods
    (cherry picked from commit ec85024)

--- WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/util/GeometryUtil.java
+++ WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/util/GeometryUtil.java
@@ -131,6 +131,119 @@ public final class GeometryUtil {
         return true;
     }
 
+    /**
+     * Visit all the points along a straight line on a 2D plane. The visitor may
+     * abort the process at any point by returning <code>false</code>.
+     *
+     * @param x1 The X coordinate of the start of the line.
+     * @param y1 The Y coordinate of the start of the line.
+     * @param x2 The X coordinate of the end of the line.
+     * @param y2 The Y coordinate of the end of the line.
+     * @param visitor The visitor to invoke for each point.
+     * @return <code>true</code> if the visitor returned true for each point
+     *     (and therefore every point was visited). <code>false</code> if the
+     *     visitor returned <code>false</code> for some point and the process
+     *     was aborted.
+     */
+    public static boolean visitLine(int x1, int y1, int x2, int y2, LineVisitor visitor) {
+        if ((x1 == x2) && (y1 == y2)) {
+            return visitor.visit(x1, y1, 0);
+        }
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        if (Math.abs(dx) > Math.abs(dy)) {
+            float y = y1, offset = (float) dy / Math.abs(dx);
+            float distance = 0, stepLength = (float) Math.sqrt(1 + ((1 + offset) * (1 + offset)));
+            dx = (dx < 0) ? -1 : 1;
+            for (int x = x1; x != x2; x += dx) {
+                if (! visitor.visit(x, (int) y, distance)) {
+                    return false;
+                }
+                y += offset;
+                distance += stepLength;
+            }
+        } else {
+            float x = x1, offset = (float) dx / Math.abs(dy);
+            float distance = 0, stepLength = (float) Math.sqrt(1 + ((1 + offset) * (1 + offset)));
+            dy = (dy < 0) ? -1 : 1;
+            for (int y = y1; y != y2; y += dy) {
+                if (! visitor.visit((int) x, y, distance)) {
+                    return false;
+                }
+                x += offset;
+                distance += stepLength;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Visit all the points along a straight line on a 2D plane. The visitor may
+     * abort the process at any point by returning <code>false</code>. Points on
+     * the line can optionally be skipped by specifying the <code>every</code>
+     * parameter larger than one.
+     *
+     * @param x1 The X coordinate of the start of the line.
+     * @param y1 The Y coordinate of the start of the line.
+     * @param x2 The X coordinate of the end of the line.
+     * @param y2 The Y coordinate of the end of the line.
+     * @param every The interval between performances of the specified task.
+     * @param visitor The visitor to invoke for each point.
+     * @return <code>true</code> if the visitor returned true for each point
+     *     (and therefore every point was visited). <code>false</code> if the
+     *     visitor returned <code>false</code> for some point and the process
+     *     was aborted.
+     */
+    public static boolean visitLine(int x1, int y1, int x2, int y2, int every, LineVisitor visitor) {
+        if ((x1 == x2) && (y1 == y2)) {
+            return visitor.visit(x1, y1, 0);
+        }
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        int count = every / 2;
+        if (Math.abs(dx) > Math.abs(dy)) {
+            float y = y1, offset = (float) dy / Math.abs(dx);
+            float distance = 0, stepLength = (float) Math.sqrt(1 + ((1 + offset) * (1 + offset)));
+            dx = (dx < 0) ? -1 : 1;
+            for (int x = x1; x != x2; x += dx) {
+                if (((count % every) == 0) && (! visitor.visit(x, (int) y, distance))) {
+                    return false;
+                }
+                y += offset;
+                distance += stepLength;
+                count++;
+            }
+        } else {
+            float x = x1, offset = (float) dx / Math.abs(dy);
+            float distance = 0, stepLength = (float) Math.sqrt(1 + ((1 + offset) * (1 + offset)));
+            dy = (dy < 0) ? -1 : 1;
+            for (int y = y1; y != y2; y += dy) {
+                if (((count % every) == 0) && (! visitor.visit((int) x, y, distance))) {
+                    return false;
+                }
+                x += offset;
+                distance += stepLength;
+                count++;
+            }
+        }
+        return true;
+    }
+
+    @FunctionalInterface
+    public interface LineVisitor {
+        /**
+         * Visit the specified absolute location along a straight line on a 2D
+         * plane.
+         *
+         * @param x The absolute X coordinate to visit.
+         * @param y The absolute Y coordinate to visit.
+         * @param d The distance from the start of the line.
+         * @return <code>true</code> if the process should continue;
+         * <code>false</code> if no more points should be visited on the line.
+         */
+        boolean visit(int x, int y, float d);
+    }
+
     @FunctionalInterface
     public interface PlaneVisitor {
         /**
