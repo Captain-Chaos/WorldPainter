b314a57ef0072cd8396a723d9a4607a4e090ac1c
Captain Chaos <codemaster@pepsoft.org>
2018-06-22 19:24:30 +0200

    Bug fix: don't try to place 0 layers of snow

--- WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/layers/exporters/FrostExporter.java
+++ WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/layers/exporters/FrostExporter.java
@@ -94,7 +94,7 @@ public class FrostExporter extends AbstractLayerExporter<Frost> implements Secon
                                             case FrostSettings.MODE_SMOOTH_AT_ALL_ELEVATIONS:
                                                 int layers = (int) ((dimension.getHeightAt(x, y) + 0.5f - dimension.getIntHeightAt(x, y)) / 0.125f) + 1;
                                                 if ((layers > 1) && (! frostEverywhere)) {
-                                                    layers = Math.max(Math.min(layers, dimension.getBitLayerCount(Frost.INSTANCE, x, y, 1) - 1), 0);
+                                                    layers = Math.max(Math.min(layers, dimension.getBitLayerCount(Frost.INSTANCE, x, y, 1) - 1), 1);
                                                 }
                                                 placeSnow(minecraftWorld, x, y, height, layers);
                                                 break;
@@ -121,6 +121,9 @@ public class FrostExporter extends AbstractLayerExporter<Frost> implements Secon
      * not already present.
      */
     private void placeSnow(MinecraftWorld minecraftWorld, int x, int y, int height, int layers) {
+        if ((layers < 1) || (layers > 8)) {
+            throw new IllegalArgumentException("layers " + layers);
+        }
         if ((minecraftWorld.getBlockTypeAt(x, y, height + 1) != BLK_SNOW) || (minecraftWorld.getDataAt(x, y, height + 1) + 1 < layers)) {
             minecraftWorld.setBlockTypeAt(x, y, height + 1, BLK_SNOW);
             minecraftWorld.setDataAt(x, y, height + 1, layers - 1);
