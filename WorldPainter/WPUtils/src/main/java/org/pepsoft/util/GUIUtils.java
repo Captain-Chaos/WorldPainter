c4b3e2222efe275059558cd2626a4b82e08794ec
Captain Chaos <codemaster@pepsoft.org>
2020-07-25 17:17:08 +0200

    Constrain automatic UI scaling to between 100% and 200%

--- WorldPainter/WPUtils/src/main/java/org/pepsoft/util/GUIUtils.java
+++ WorldPainter/WPUtils/src/main/java/org/pepsoft/util/GUIUtils.java
@@ -196,7 +196,7 @@ public class GUIUtils {
      * <p><strong>Note:</strong> for now UI scaling is only activated on <!-- TODO -->
      * Windows, until the current support on Mac and Linux can be investigated. <!-- TODO -->
      */
-    public static final float SYSTEM_UI_SCALE_FLOAT = SystemUtils.isWindows() ? (float) Toolkit.getDefaultToolkit().getScreenResolution() / 96 : 1.0f;
+    public static final float SYSTEM_UI_SCALE_FLOAT = SystemUtils.isWindows() ? MathUtils.clamp(1.0f, (float) Toolkit.getDefaultToolkit().getScreenResolution() / 96, 2.0f) : 1.0f;
 
     /**
      * How many times to scale pixel sizes to display at approximately the
