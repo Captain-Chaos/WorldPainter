3c034578bfdc3f45e26be02d776a96df3abdc513
Captain Chaos <codemaster@pepsoft.org>
2021-12-08 17:17:05 +0100

    Provide compatibility post processor for existing plugins

---
+++ WorldPainter/WPCore/src/main/java/org/pepsoft/worldpainter/exporting/Java1_2PostProcessor.java
@@ -0,0 +1,9 @@
+package org.pepsoft.worldpainter.exporting;
+
+/**
+ * @deprecated Use {@link org.pepsoft.worldpainter.platforms.Java1_2PostProcessor}.
+ */
+@Deprecated
+public class Java1_2PostProcessor extends org.pepsoft.worldpainter.platforms.Java1_2PostProcessor {
+    // This class is here for backwards compatibility with existing external plugins
+}
