cd8da5f7e61f2615f53b7865328930d5aeebd5f0
Captain Chaos <codemaster@pepsoft.org>
2022-04-22 11:23:26 +0200

    Don't scale GUI in safe mode

--- WorldPainter/WPUtils/src/main/java/org/pepsoft/util/GUIUtils.java
+++ WorldPainter/WPUtils/src/main/java/org/pepsoft/util/GUIUtils.java
@@ -27,17 +27,6 @@ import static java.lang.Math.round;
  */
 public class GUIUtils {
     /**
-     * Override the detected system default UI scale.
-     *
-     * @param uiScale The UI scale to use instead of the detected system
-     *                default.
-     */
-    public static void setUIScale(float uiScale) {
-        UI_SCALE_FLOAT = uiScale;
-        UI_SCALE = round(uiScale);
-    }
-
-    /**
      * Scale an image according to {@link #UI_SCALE}. Nearest neighbour scaling
      * is used, in other words no smoothing or interpolation is applied.
      *
@@ -206,26 +195,15 @@ public class GUIUtils {
     }
 
     /**
-     * The detected system default UI scale.
+     * Override the detected system default UI scale.
      *
-     * <p><strong>Note:</strong> for now UI scaling is only activated on <!-- TODO -->
-     * Windows, until the current support on Mac and Linux can be investigated. <!-- TODO -->
+     * @param uiScale The UI scale to use instead of the detected system
+     *                default.
      */
-    public static final float SYSTEM_UI_SCALE_FLOAT = SystemUtils.isWindows() ? MathUtils.clamp(1.0f, (float) Toolkit.getDefaultToolkit().getScreenResolution() / 96, 2.0f) : 1.0f;
-
-    /**
-     * How many times to scale pixel sizes to display at approximately the
-     * originally intended size for assets which were designed for 92-96 dpi
-     * screens.
-     *
-     * <p>This is {@link #UI_SCALE_FLOAT} rounded to the nearest integer and is
-     * intended for small images that don't scale well to non integer factors
-     * such as icons.
-     *
-     * <p><strong>Note:</strong> for now UI scaling is only activated on <!-- TODO -->
-     * Windows, until the current support on Mac and Linux can be investigated. <!-- TODO -->
-     */
-    public static final int SYSTEM_UI_SCALE = round(SYSTEM_UI_SCALE_FLOAT);
+    public static void setUIScale(float uiScale) {
+        UI_SCALE_FLOAT = uiScale;
+        UI_SCALE = round(uiScale);
+    }
 
     /**
      * How many times to scale pixel sizes to display at approximately the
@@ -255,8 +233,41 @@ public class GUIUtils {
         return UI_SCALE;
     }
 
-    private static float UI_SCALE_FLOAT = SYSTEM_UI_SCALE_FLOAT;
-    private static int UI_SCALE = SYSTEM_UI_SCALE;
+    /**
+     * The detected system default UI scale.
+     *
+     * <p><strong>Note:</strong> for now UI scaling is only activated on <!-- TODO -->
+     * Windows, until the current support on Mac and Linux can be investigated. <!-- TODO -->
+     */
+    public static final float SYSTEM_UI_SCALE_FLOAT;
+
+    /**
+     * How many times to scale pixel sizes to display at approximately the
+     * originally intended size for assets which were designed for 92-96 dpi
+     * screens.
+     *
+     * <p>This is {@link #UI_SCALE_FLOAT} rounded to the nearest integer and is
+     * intended for small images that don't scale well to non integer factors
+     * such as icons.
+     *
+     * <p><strong>Note:</strong> for now UI scaling is only activated on <!-- TODO -->
+     * Windows, until the current support on Mac and Linux can be investigated. <!-- TODO -->
+     */
+    public static final int SYSTEM_UI_SCALE;
 
     private static final Logger logger = LoggerFactory.getLogger(GUIUtils.class);
+
+    static {
+        if ("true".equalsIgnoreCase(System.getProperty("org.pepsoft.worldpainter.safeMode"))) {
+            logger.info("[SAFE MODE] Not scaling GUI");
+            SYSTEM_UI_SCALE_FLOAT = 1.0f;
+            SYSTEM_UI_SCALE = 1;
+        } else {
+            SYSTEM_UI_SCALE_FLOAT = SystemUtils.isWindows() ? MathUtils.clamp(1.0f, (float) Toolkit.getDefaultToolkit().getScreenResolution() / 96, 2.0f) : 1.0f;
+            SYSTEM_UI_SCALE = round(SYSTEM_UI_SCALE_FLOAT);
+        }
+    }
+
+    private static float UI_SCALE_FLOAT = SYSTEM_UI_SCALE_FLOAT;
+    private static int UI_SCALE = SYSTEM_UI_SCALE;
 }
